5a942b72ea36993c5b54998ee512c20d3d774395
Cristiano Santos <cristiano.santos@jusbrasil.com.br>
2024-10-05 09:35:23 -0300

    chore(db): use Mongo URI

--- python_o_bom_candidato/app.py
+++ python_o_bom_candidato/app.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+
 from .crawler import crawler_resources
 from .data_processing import consulta_cand, bens_candidato
 from .db_operations import search_candidate, insert_basic_info_db, insert_bens_info_db, get_picture_from_server, search_unique_number
@@ -14,23 +15,28 @@ app = FastAPI()
 app.mount("/o_bom_candidato_files", StaticFiles(directory="./o_bom_candidato_files"), name="o_bom_candidato_files")
 
 @app.get("/search", response_class=HTMLResponse)
-async def search_candidates(request:Request, candidate_to_search:str):
+def search_candidates(request:Request, candidate_to_search:str):
     if not candidate_to_search.strip():
         context = {'request': request}
         return templates.TemplateResponse("candidate_info.html", context)
-    candidates = await get_picture_from_server(search_candidate(candidate_to_search))
-    return templates.TemplateResponse("candidate_info.html", {"request":request, "candidates":candidates})
+
+    candidates = search_candidate(candidate_to_search)
+    candidates_with_picture = get_picture_from_server(candidates)
+    return templates.TemplateResponse("candidate_info.html", {"request":request, "candidates":candidates_with_picture})
+
 
 @app.get('/', response_class=HTMLResponse)
 def index(request: Request):
     context = {'request': request}
     return templates.TemplateResponse("index.html", context)
 
+
 @app.get('/candidate_page/{unique_number}', response_class=HTMLResponse)
-async def candidate_page(request:Request, unique_number:str):
-    candidate = await get_picture_from_server(dict(search_unique_number(unique_number=unique_number)))
+def candidate_page(request:Request, unique_number:str):
+    candidate = get_picture_from_server(dict(search_unique_number(unique_number=unique_number)))
     print(candidate)
     return templates.TemplateResponse("candidate_card.html", {"request":request, "candidate":candidate})
+
 
 @app.post("/crawl_candidates_basic_info/")
 def update_candidates_basic_info():
